62f47a054314a54d399bbf007bfee1f004af231d
Bryan <bryan.t.lin1@gmail.com>
2012-07-24 22:03:29 -0400

    Fixed some issues and took out execution code.
    This module is now just a class to use for any evernote client

--- evernote_oauth_test.py
+++ evernote_oauth_test.py
@@ -1,4 +1,4 @@
-#!C:/Python27/python.exe
+#!/usr/bin/python
 # This simple script will enable the oauth callbacks to the Evernote OAuth
 # services. As of now, the Evernote OAuth url is using the Evernote sandbox servers.
 # Be sure to fill in your callback url as well as your consumer key and secret key.
@@ -14,10 +14,10 @@ import oauth2 as oauth
 class EvernoteOAuth(object):
     CONSUMER_KEY = "CONSUMER_KEY"
     CONSUMER_SECRET = "CONSUMER_SECRET"
-    OAUTH_TOKEN_URL = "https://sandbox.evernote.com/oauth"
-    AUTHORIZE_URL = "https://sandbox.evernote.com/OAuth.action"
+    OAUTH_TOKEN_URL = "https://www.evernote.com/oauth"
+    AUTHORIZE_URL = "https://www.evernote.com/OAuth.action"
 
-    def __init__(self, temp_token, verifier, signature_method):
+    def __init__(self, temp_token=None, verifier=None, signature_method=oauth.SignatureMethod_PLAINTEXT()):
         """
         Constructs the EvernoteOAuth class by creating consumer and client.
         The token is created if an existing token and verifier was passed in
@@ -38,7 +38,7 @@ class EvernoteOAuth(object):
                                            secret=EvernoteOAuth.CONSUMER_SECRET)
             #Create a token from the temporary token to be authorized
             self.token = oauth.Token(temp_token,'')
-            self.token.set_verifier(self.consumer, self.token)
+            self.token.set_verifier(verifier)
             self.client = oauth.Client(self.consumer, self.token)
             self.client.set_signature_method(signature_method)
             
@@ -81,26 +81,3 @@ class EvernoteOAuth(object):
         """
         return EvernoteOAuth.AUTHORIZE_URL + "?oauth_token=%s" % request_token
     
-
-if __name__ == "__main__":
-    callback_url = "CALLBACK_URL"
-
-    try:
-                    
-        #Get the values for token, verifier, and signature method if provided
-        token =  sys.argv[0] if len(sys.argv) > 3 else None
-        verifier = sys.argv[1] if len(sys.argv) > 3 else None
-        signature_method = sys.argv[2] if len(sys.argv) > 3 else oauth.SignatureMethod_PLAINTEXT()
-    
-        #Get the token from Evernote
-        evernote = EvernoteOAuth(token, verifier, signature_method)
-        if token != None and verifier != None:
-            request_token = evernote.get_access_token()
-        else:
-            #Get the redirected url from the returned temporary access token
-            request_token = evernote.get_request_token(callback_url)
-            redirect_url = evernote.get_redirect_url(request_token['oauth_token'])
-            print "Login into evernote with this url: %s" % redirect_url
-    except Exception, e:
-        print "Invalid Response : %s" % e
-
